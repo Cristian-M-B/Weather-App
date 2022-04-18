bed431b2f2128982732cda7093f89ac3f6d89c21
Cristian-M-B <crisbaronetto@hotmail.com>
2022-04-18 14:43:50 -0300

    add darkMode button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import { useDispatch, useStore } from '../context/StoreProvider';
+import { actionsList } from '../context/StoreReducer';
 import SearchBar from './SearchBar';
-import { AppBar, Toolbar, Grid, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Grid, Typography, IconButton } from '@material-ui/core';
+import { WbSunny, Brightness2 } from '@material-ui/icons';
 
 export default function Nav(): JSX.Element {
+    const dispatch = useDispatch();
+    const { darkMode } = useStore();
+
+    function handleClick(e: React.MouseEvent<HTMLButtonElement>): void {
+        dispatch({ type: actionsList.CHANGE_MODE });
+    }
+
     return (
         <AppBar position='static'>
-            <Toolbar style={{padding:'1vh'}}>
+            <Toolbar style={{ padding: '1vh' }} color='primary'>
                 <Grid
                     container
                     direction='row'
                     justifyContent='space-around'
                     alignItems='center'
                 >
-                    <Typography>Weather App</Typography>
+                    <Typography>Weather</Typography>
                     <SearchBar />
-                    <Typography>App</Typography>
+                    <IconButton onClick={handleClick} style={{ color: darkMode ? 'white' : 'black' }} >
+                        {darkMode ?
+                            <WbSunny /> :
+                            <Brightness2 />
+                        }
+                    </IconButton>
                 </Grid>
             </Toolbar>
         </AppBar>

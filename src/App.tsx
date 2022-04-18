8543a48398e6bbc5236d9793743667909b56ffff
Cristian-M-B <crisbaronetto@hotmail.com>
2022-04-18 14:41:06 -0300

    add context and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useStore } from './context/StoreProvider';
+import { actionsList } from './context/StoreReducer';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import { ThemeProvider } from '@material-ui/styles';
+import { lightTheme, darkTheme } from './theme';
 
 export default function App(): JSX.Element {
+  const dispatch = useDispatch();
+  const { darkMode } = useStore();
+
+  useEffect(() => {
+    dispatch({ type: actionsList.GET_LOCAL_STORAGE });
+  }, [])
+
   return (
-    <>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Nav />
       <Home />
-    </>
+    </ThemeProvider>
   )
 }
